407907aa32bfee3dda0dde3a25ab002381f21ca1
phuongvo1110 <vptphuong21@clc.fitus.edu.vn>
2025-01-03 22:39:00 +0700

    deleting files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
-import { NotfoundComponent } from "./demo/components/notfound/notfound.component";
 import { AppLayoutComponent } from "./layout/app.layout.component";
 import { AuthGuard } from "./helpers/auth.guard";
 
@@ -13,62 +12,12 @@ import { AuthGuard } from "./helpers/auth.guard";
           component: AppLayoutComponent,
           children: [
             {
-              path: "dashboard",
-              loadChildren: () =>
-                import("./demo/components/dashboard/dashboard.module").then(
-                  (m) => m.DashboardModule
-                ),
-              canActivate: [AuthGuard],
-            },
-            {
               path:'',
               loadChildren: () => import('./pages/pages.module').then((m) => m.PagesModule),
               canActivate: [AuthGuard]
             },
-            {
-              path: "uikit",
-              loadChildren: () =>
-                import("./demo/components/uikit/uikit.module").then(
-                  (m) => m.UIkitModule
-                ),
-            },
-            {
-              path: "utilities",
-              loadChildren: () =>
-                import("./demo/components/utilities/utilities.module").then(
-                  (m) => m.UtilitiesModule
-                ),
-            },
-            {
-              path: "pages",
-              loadChildren: () =>
-                import("./demo/components/pages/pages.module").then(
-                  (m) => m.PagesModule
-                ),
-            },
           ],
-        },
-        {
-          path: "authenticate",
-          loadChildren: () =>
-            import("./account/account.module").then((m) => m.AccountModule),
-        },
-        {
-          path: "auth",
-          loadChildren: () =>
-            import("./demo/components/auth/auth.module").then(
-              (m) => m.AuthModule
-            ),
-        },
-        {
-          path: "landing",
-          loadChildren: () =>
-            import("./demo/components/landing/landing.module").then(
-              (m) => m.LandingModule
-            ),
-        },
-        { path: "notfound", component: NotfoundComponent },
-        { path: "**", redirectTo: "/notfound" },
+        }
       ],
       {
         scrollPositionRestoration: "enabled",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,6 @@ import {
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppLayoutModule } from "./layout/app.layout.module";
-import { NotfoundComponent } from "./demo/components/notfound/notfound.component";
-import { ProductService } from "./demo/service/product.service";
-import { CountryService } from "./demo/service/country.service";
-import { CustomerService } from "./demo/service/customer.service";
-import { EventService } from "./demo/service/event.service";
-import { IconService } from "./demo/service/icon.service";
-import { NodeService } from "./demo/service/node.service";
-import { PhotoService } from "./demo/service/photo.service";
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { JwtInterceptor } from "./helpers/jwt.interceptor";
 import { ErrorInterceptor } from "./helpers/error.interceptor";
@@ -23,19 +15,12 @@ import { AccountService } from "./services/account.service";
 import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent, NotfoundComponent],
+  declarations: [AppComponent],
   imports: [AppRoutingModule, AppLayoutModule, HttpClientModule, SharedModule, ReactiveFormsModule],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     { provide: LocationStrategy, useClass: PathLocationStrategy },
-    CountryService,
-    CustomerService,
-    EventService,
-    IconService,
-    NodeService,
-    PhotoService,
-    ProductService,
     MessageService
   ],
   bootstrap: [AppComponent],
